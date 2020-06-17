c685d57a179fa983a43f36fe6087126529950cc4
Nipun Mohindra <66218859+nipunmohindra@users.noreply.github.com>
2020-06-17 11:48:35 +0530

    Add files via upload

---
+++ two.py
@@ -0,0 +1,12 @@
+#two.py
+import one
+
+print("TOP LEVEL IN TWO.PY")
+
+one.func()
+
+if __name__=='__main__':
+	print("TWO.PY is being run directly!")
+else:
+	print("TWO.PY has been imported!")
+	
